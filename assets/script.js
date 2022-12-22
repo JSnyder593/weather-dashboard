6dff2efdb4ad79257321c2ecae15fe25551d25dd
JSnyder593 <jaeger_snyder@hotmail.com>
2022-12-21 21:31:20 -0800

    card comes up, day of forecast shows up

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,31 @@
 //global variables
 var submitBtn = document.getElementById('submit-button');
 var currentWeather = $('#weather');
-var weekForecast = $('#weeks-forecast');
+var fiveDayForecast = $('#weeks-forecast');
 
 //setup API key
-var APIKey = "4d23d9d4be9f729849f35822048b70d9"
+var APIKey = "f870f861932b4a0d394d100e06527d69"
 
 //gets current date
 var today = moment();
 $('#current-date').text(today.format('MMM DD, YYYY'));
 
 //stores user input as a value in local storage
-function returnCityName() {
-    let cityInput = document.getElementById("city-input").value;
-    console.log(cityInput)
+function returnCity() {
+    const cityInput = document.getElementById("city-input").value;
+
+    weatherToday(cityInput)
+    weeksForecast(cityInput)
 
     if (cityInput) {
         localStorage.setItem("city", cityInput);
-        retrieveWeather();
     }
+
 };
 
-//gets current weather info based on the users input
-function retrieveWeather() {
-    var city = localStorage.getItem("city")
-    let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appID=" + APIKey + "&units=imperial";
+// //gets current weather info based on the users input
+function weatherToday(cityInput) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appID=" + APIKey + "&units=imperial";
 
     console.log(queryURL);
 
@@ -33,7 +34,6 @@ function retrieveWeather() {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
             currentWeather.append(`<h2>${data.name}</h2>`);
             currentWeather.append(`<p><img src="https://openweathermap.org/img/wn/${(data.weather[0].icon)}.png"></img></p>`);
             currentWeather.append(`<p>Temp: <span>${data.main.temp}°F</span></p>`);
@@ -44,23 +44,51 @@ function retrieveWeather() {
 
 // gets five day forecast based on users input
 function weeksForecast() {
-    var city = localStorage.getItem("city")
-    let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appID=" + APIKey + "&units=imperial";
 
-    fetch(queryURL)
+    var cityInput = localStorage.getItem("city")
+    let fiveDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appID=" + APIKey + "&units=imperial";
+
+
+    fetch(fiveDayURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
-            var weeksArray = data.list;
-            console.log(weeksArray)
-
-            for (var i = 2; i < weeksArray.length; i += 8) {
-                
-                weekForecast.append(`<div class="col-2 border border-secondary m-1 bg-dark text-white"><p>${moment(forecast.dt_txt).format('MMM DD, YYYY')}</p><p><img src="https://openweathermap.org/img/wn/${(forecast.weather[0].icon)}.png"></img></p><p>Temp: <span>${forecast.main.temp}°F</span></p><p>Wind: <span>${forecast.wind.speed}MPH</span></p><p>Humidity: <span>${forecast.main.humidity}%</span></p></div>`);
-            }
+            console.log(data);
+            
+            fiveDayForecast.append(`<div class="col-2 border border-secondary m-1 bg-dark text-white"><p>${moment(data.main.dt_txt).format('MMM DD, YYYY')}</p><p><img src="https://openweathermap.org/img/wn/${(data.weather[0].icon)}.png"></img></p><p>Temp: <span>${data.main.temp}°F</span></p><p>Wind: <span>${data.wind.speed}MPH</span></p><p>Humidity: <span>${data.main.humidity}%</span></p></div>`);
         })
-}
+        
+    
+    
+    
+    
+    
+    // forecast => {
+    //         forecast.list.array.forEach(day => {
+            
+    //         const dayContainer = document.createElement('div')
 
-submitBtn.addEventListener('click', returnCityName);
+    //         const temp = day.main.temp;
+    //         const tempElem = document.createElement('div')
+    //         tempElem.innerText = Math.round(temp) + '°F'
+    //         dayContainer.appendChild(tempElem)
+
+    //         const icon = day.weather[0].icon;
+    //         const iconElem = document.createElement('img')
+    //         iconElem.src = 'http://openweathermap.org/img/wn/' + icon + '.png'
+    //         dayContainer.appendChild(iconElem)
+
+    //         document.body.appendChild(dayContainer, '#weeks')
+    //     })
+    // })
+
+        //     for (let i = 2; i < fiveDay.length; i+=8) {
+
+                
+
+        //     }
+        // })
+};
+
+submitBtn.addEventListener('click', returnCity);
